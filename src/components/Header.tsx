bab7e08011a8479be68ec6c25b1ac916e20e7f39
Aleja <tu@email.com>
2026-07-29 19:06:58 -0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="bg-blue-700 text-white p-5 shadow">
+            <h1 className="text-2xl font-bold">
+                Sistema de Gestion de Tours
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/interfaces/Tour.ts
@@ -0,0 +1,7 @@
+export interface Tour{
+    _id?: string;
+    nombre: string;
+    destino: string;
+    precio: number;
+    duracion: string;
+}
